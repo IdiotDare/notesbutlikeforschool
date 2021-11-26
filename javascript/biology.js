5729d87677e9ade089979c24dc414e3f480d2963
IdiotDare <89470696+IdiotDare@users.noreply.github.com>
2021-11-26 13:49:33 -0500

    Update biology.js

--- javascript/biology.js
+++ javascript/biology.js
@@ -1,10 +1,9 @@
 function myName(){
   let content = "yeet";
-  let secCon = document.getElementById("myname").innerHTML
+  let secCon = "yeet again";
   if(document.getElementById("myname").innerHTML != content){
     document.getElementById("myname").innerHTML = content;
-    content = secCon;
   } else{
-  document.getElementById("myname").innerHTML = content
+  document.getElementById("myname").innerHTML = secCon;
   }
 }
